8884493bd4cb9e8e9ddd3ad289155c48de701ab5
lian090312 <lian090312@gmail.com>
2022-10-09 22:20:57 +0900

    second commit

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,14 +1,43 @@
-// 사용변수
-let score = 0;
+const container = document.querySelector(".image-container");
+const startButton = document.querySelector(".start-button");
+const gameText = document.querySelector(".game-text");
+const playTime = document.querySelector(".play-time");
 
-const wordInput = document.querySelector(".word-input");
-const wordDisplay = document.querySelector(".word-display");
-const scoreDisplay = document.querySelector(".score");
+const tileCount = 16;
 
-wordInput.addEventListener("input", () => {
-  if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
-    score++;
-    scoreDisplay.innerText = score;
-    wordInput.value = "";
+let tiles = [];
+
+setGame();
+
+function setGame() {
+  container.innerHTML = "";
+  tiles = createImageTiles();
+  tiles.forEach((tile) => container.appendChild(tile));
+  setTimeout(() => {
+    container.innerHTML = "";
+    shuffle(tiles).forEach((tile) => container.appendChild(tile));
+  }, 2000);
+}
+
+function createImageTiles() {
+  const tempArray = [];
+  Array(tileCount)
+    .fill()
+    .forEach((_, i) => {
+      const li = document.createElement("li");
+      li.setAttribute("data-index", i);
+      li.classList.add(`list${i}`);
+      tempArray.push(li);
+    });
+  return tempArray;
+}
+
+function shuffle(array) {
+  let index = array.length - 1;
+  while (index > 0) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+    index--;
   }
-});
+  return array;
+}
