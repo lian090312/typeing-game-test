718545dab9c1e4455f6c46421e699e364582d09c
lian090312 <lian090312@gmail.com>
2022-10-09 11:55:50 +0900

    second commit

---
+++ src/JS/main.js
@@ -0,0 +1,14 @@
+// 사용변수
+let score = 0;
+
+const wordInput = document.querySelector(".word-input");
+const wordDisplay = document.querySelector(".word-display");
+const scoreDisplay = document.querySelector(".score");
+
+wordInput.addEventListener("input", () => {
+  if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+    score++;
+    scoreDisplay.innerText = score;
+    wordInput.value = "";
+  }
+});
